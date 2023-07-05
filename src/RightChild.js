213989b7f7c1ef5dd58f7166220bc16f266a2cde
Apekshasoni06 <84591271+Apekshasoni06@users.noreply.github.com>
2023-07-05 09:31:28 +0530

    Update RightChild.js

--- src/RightChild.js
+++ src/RightChild.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import Data from './data';
+import React from "react";
 
-const RightChild = ({ id }) => {
-  const selectedItem = Data.find((item) => item.id === id);
-
+const RightChild = ({ selectedItem }) => {
   if (!selectedItem) {
     return <div>No item found</div>;
   }
 
   return (
     <div>
-      <h1 class="h1">Cards:</h1>
-    <div class="box">
-    <h2>{selectedItem.Name}</h2>
-    <p>Price: {selectedItem.Price}</p>
-      
-      <h3>Item id: {selectedItem.id}</h3>
-     
-      
-    </div>
+      <h1 className="h1">Cards:</h1>
+      <div className="box">
+        <h2>{selectedItem.Name}</h2>
+        <p>Price: {selectedItem.Price}</p>
+        <h3>Item id: {selectedItem.id}</h3>
+      </div>
     </div>
   );
 };
